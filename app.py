e079e829b7212ba639149e5ba787ffd145de49e8
Linden Kyaw <kyawlinden@gmail.com>
2026-02-15 16:13:04 -0800

    Fix app.py formatting with black

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import random
 import string
 import os
 
-BASE_URL = os.environ.get('BASE_URL', 'http://127.0.0.1:5000')
+BASE_URL = os.environ.get("BASE_URL", "http://127.0.0.1:5000")
+
 
 class InputURL(BaseModel):
     """
